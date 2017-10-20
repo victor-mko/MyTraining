a7155ca65a2615532395ccbda02f701f0a1855cf
Viktor Mihailenko <viktormihailenko1990@gmail.com>
2017-10-20 21:51:32 +0300

    [MyTraining] counter is created

--- project/src/App.js
+++ project/src/App.js
@@ -3,13 +3,23 @@ import Increment from './Incriment';
 import { connect } from "react-redux";
 
 class App extends Component {
-  
+  constructor(props){
+    super(props);
+    this.addOne = this.addOne.bind(this);
+    this.removeOne = this.removeOne.bind(this);
+  }
+  addOne(){
+    this.props.onAddOne();
+  }
+  removeOne(){
+    this.props.onRemoveOne();
+  }
   render () {
-    console.log(this.props.testStore);
     return (
       <div> 
-        <Increment />
+        <button onClick={this.addOne}>+1</button>
         <div>{this.props.testStore}</div>
+        <button onClick={this.removeOne}>-1</button>
       </div>
     )
   }
@@ -21,5 +31,12 @@ export default connect(
   state => ({
     testStore: state
   }),
-  dispatch => ({})
+  dispatch => ({
+    onAddOne: () => {
+      dispatch({ type: 'INCREMENT'})
+    },
+    onRemoveOne: () => {
+      dispatch({ type: 'DECREMENT'})
+    }
+  })
 )(App);
